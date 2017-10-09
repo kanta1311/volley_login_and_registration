8ed2d36811efe0b75e0c35b849eb4a4da2036a54
Arvind Kant <kanta1311@gmail.com>
2017-10-09 10:47:20 +0530

    initial commit

---
+++ app/src/main/java/com/kantapp/instagram/URLs.java
@@ -0,0 +1,14 @@
+package com.kantapp.instagram;
+
+/**
+ * Created by Belal on 9/5/2017.
+ */
+
+public class URLs {
+
+    private static final String ROOT_URL = "https://kantapp.xyz/zeist/volley/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+
+}
